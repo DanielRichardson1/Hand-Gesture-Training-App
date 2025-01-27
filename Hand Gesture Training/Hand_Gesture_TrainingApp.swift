e6380aca786205ab1f1ec835b3abacb43435bfe3
Daniel Richardson <drich1475@gmail.com>
2025-01-27 15:23:14 -0500

    Initial Commit

---
+++ Hand Gesture Training/Hand_Gesture_TrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hand_Gesture_TrainingApp.swift
+//  Hand Gesture Training
+//
+//  Created by Daniel Richardson on 1/27/25.
+//
+
+import SwiftUI
+
+@main
+struct Hand_Gesture_TrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
