aa106ed240be6df04eeb8cf4fda32dfbd8ff7898
Daniel Richardson <drich1475@gmail.com>
2025-01-29 14:38:47 -0500

    CalibrationView Stuff

---
+++ Hand Gesture Training/Model/GestureType.swift
@@ -0,0 +1,8 @@
+//
+//  GestureType.swift
+//  Hand Gesture Training
+//
+//  Created by Daniel Richardson on 1/29/25.
+//
+
+import Foundation
---
+++ Hand Gesture Training/ViewModels/UpdateViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UpdateViewModel.swift
+//  Hand Gesture Training
+//
+//  Created by Daniel Richardson on 1/29/25.
+//
+
+import Foundation
